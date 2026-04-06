dcadb0dc7fc5a60a66879967ac230bc99a60671d
unknown <lochaydeguerreiro@hotmail.com>
2026-04-06 18:46:21 -0300

    commit 16

--- config/conexao.php
+++ config/conexao.php
@@ -3,8 +3,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$host = 'ftp-loxaid.alwaysdata.net';
-$db   = 'loxaid_agencia_led';
+// O segredo está no prefixo 'mysql-'
+$host = 'mysql-loxaid.alwaysdata.net'; 
+$db   = 'loxaid_agencia_led'; // Verifique se o nome no painel tem os underlines exatamente assim
 $user = 'loxaid';
 $pass = 'Q2fcMq*XZ5S*t35';
 $charset = 'utf8mb4';
@@ -19,7 +20,9 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Conexão realizada com sucesso!"; // Descomente para testar
 } catch (\PDOException $e) {
+    // Em produção, é melhor não mostrar o erro bruto para o usuário, mas para o log
     error_log($e->getMessage());
     exit("Erro técnico: Não foi possível conectar ao banco de dados.");
 }
